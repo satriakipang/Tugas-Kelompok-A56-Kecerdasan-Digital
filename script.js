c824f63f98dce730152acf0df501e1c3df3e62ba
satriakipang <satriakipang@gmail.com>
2021-10-22 13:26:03 +0700

    text

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function fixNav() {
         nav.classList.remove('active')
     }
 }
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
